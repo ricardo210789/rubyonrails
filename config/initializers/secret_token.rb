9e2c08fadfc90c66fcd87e7d941cbd3a7660f21e
Ricardo Avila Legrá <ricardo210789@gmail.com>
2014-07-11 12:15:54 +0200

    Signed-off-by: Ricardo Avila Legrá <ricardo210789@gmail.com>

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rail::Application.config.secret_token = 'ac65c59bc62cbf44e17496af576ff5b6c77dc9c858b6f72e544d41157ab11cf3598136b576b46266b6b317b0980ebf6cc7abb46430796cb0df653e05b0330c5f'
